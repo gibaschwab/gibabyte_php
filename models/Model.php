5f0cce2a3bf361e348c76c4473d3807c73893033
Jean Roberto Schwab <giba_schwab@hotmail.com>
2023-03-28 20:41:24 -0300

    models inverval

--- models/Model.php
+++ models/Model.php
@@ -1,5 +1,38 @@
 <?php
 
 class Model {
-    
+    private $driver = 'mysql';
+    private $host = 'localhost';
+    private $dbname = 'exemplodsi';
+    private $user = 'root';
+    private $password = null;
+
+    // var conexão (PDO)
+    private $conex;
+
+    // nome table
+    protected $table;
+
+    public function __construct()
+    {
+        $this->conex = new PDO("{$this->driver}:host={$this->host};dbname={$this->dbname}",$this->user,$this->password);
+
+        // descobre o nome da tabela
+        $tbl = strtolower(get_class($this));
+        $tbl = $tbl . 's'; //add  's' no final
+        $this->table = $tbl;
+    }
+
+    public function getById($id) {
+        $sql = $this->conex->prepare("SELECT * FROM {$this->table} where id = ?");
+        $sql->bindParam(1, $id);
+        $sql->execute();
+        return $sql->fetch(PDO::FETCH_ASSOC);
+        ;
+    }
+
+    public function getAll() {
+        $sql = $this->conex->query("SELECT * FROM {$this->table}");
+        return $sql->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
--- produtos.php
+++ produtos.php
@@ -1,14 +1,10 @@
 <?php
-    // produtos.php
-    include('variaveis.php');
-    require('head.php');
-?>
-    <h1><?= $titulo; ?></h1>
-    <p><?= $msg; ?></p>
-    <h2>Produtos</h2>
-    <ul>
-        <li>Produto 1</li>
-        <li>Produto 2</li>
-    </ul>
-</body>
-</html>
+
+require('models/Model.php');
+require('models/Produto.php');
+
+$prod = new Produto();
+
+$resultado = $prod->getAll();
+
+var_dump($resultado);
